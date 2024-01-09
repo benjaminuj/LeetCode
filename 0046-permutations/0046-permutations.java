46d47209bc9c159240da5c372557722d74d13772
YuJin Cho <chosj1526@gmail.com>
2024-01-09 13:21:48 +0900

    Time: 1 ms (95.77%), Space: 44.8 MB (5.94%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -2,14 +2,14 @@ class Solution {
     List<List<Integer>> answer = new ArrayList<List<Integer>>();
 
     public List<List<Integer>> permute(int[] nums) {
-        backtracking(nums, 0, new ArrayList<Integer>(), new boolean[nums.length]);
+        backtracking(nums, new ArrayList<Integer>(), new boolean[nums.length]);
 
         return answer;
     }
 
-    public void backtracking(int[] nums, int number, List<Integer> current, boolean[] visited) {
+    public void backtracking(int[] nums, List<Integer> current, boolean[] visited) {
         //basecase
-        if (number == nums.length) {
+        if (current.size() == nums.length) {
             answer.add(new ArrayList<>(current));
             return;
         }
@@ -18,12 +18,11 @@ class Solution {
             if (!visited[i]) {
                 visited[i] = true;
                 current.add(nums[i]);
-                backtracking(nums, number+1, current, visited);
+                backtracking(nums, current, visited);
                 
                 visited[i] = false;
                 current.remove(current.size() - 1);
             }
-            
         }
     }
 }
