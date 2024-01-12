4e19b1cab4bc8eb8b5ead30675bcabfedb3ea5aa
YuJin Cho <chosj1526@gmail.com>
2024-01-12 15:06:29 +0900

    Time: 1 ms (98.63%), Space: 41.1 MB (45.17%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,27 +2,22 @@ import java.util.*;
 
 class Solution {
     public boolean isValid(String s) {
-        Deque<Character> q = new ArrayDeque<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char now = s.charAt(i);
-            
-            if (now == '(') {
-                q.push(')');
-            } else if (now == '{') {
-                q.push('}');
-            } else if (now == '[') {
-                q.push(']');
-            } else if (!q.isEmpty() && q.peek() == now) {
-                q.pop();
+        Deque<Character> stack = new ArrayDeque<>();
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
             } else {
                 return false;
             }
         }
         
-        if (q.isEmpty()) {
-            return true;
-        }
-        return false;
+        return stack.isEmpty();
     }
 }
