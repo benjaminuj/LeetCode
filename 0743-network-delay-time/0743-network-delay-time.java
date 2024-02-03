f737c009d1552f647366fa2e3013d40ba967db89
YuJin Cho <chosj1526@gmail.com>
2024-02-04 02:16:52 +0900

    Time: 19 ms (49.38%), Space: 45.8 MB (97.19%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,72 +1,32 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        int answer = 0;
-        int[] distance = new int[n+1];
-        Arrays.fill(distance, Integer.MAX_VALUE);
-        distance[0] = 0;
-        Map<Integer, List<Entry>> graph = new HashMap<>();
+        Map<Integer, List<int[]>> edges = Arrays.stream(times).collect(Collectors.groupingBy(t -> t[0]));
+        int[] visited = new int[n+1];
+        Arrays.fill(visited, Integer.MAX_VALUE);
         
-        for (int i =0 ; i < times.length; i++) {
-            List<Entry> temp = graph.get(times[i][0]);
+        Queue<int[]> pq = new PriorityQueue<>((e1, e2) -> (e1[1] - e2[1]));
+        pq.add(new int[]{k, 0});
+        visited[k] = 0;
+        
+        int maxTime = 0;
+        int visitCount = 1;
+        while(!pq.isEmpty()) {
+            int[] cur = pq.remove();
+            int u = cur[0], time = cur[1];
+            if (visited[u] < time) continue;
+            maxTime = time;
             
-            if (temp == null) {
-                temp = new ArrayList<>();
-                graph.put(times[i][0], temp);
-            }
-            
-            temp.add(new Entry(times[i][1], times[i][2])); // 도착지, 시간
+            if (!edges.containsKey(u)) continue;
+            for (int[] edge : edges.get(u)) {
+                int v = edge[1], w = edge[2];
+                
+                if (time + w >= visited[v]) continue;
+                
+                if (visited[v] == Integer.MAX_VALUE) visitCount++;
+                visited[v] = time + w;
+                pq.add(new int[]{v, time + w});
+            } 
         }
-        
-        Queue<Entry> q = new PriorityQueue<>();
-        q.offer(new Entry(k, 0)); // 현재 노드, 현재 노드까지 누적 걸린시간
-        distance[k] = 0;
-        while(!q.isEmpty()) {
-            Entry cur = q.poll();
-            if (distance[cur.node] < cur.time) continue;
-            
-            if (graph.get(cur.node) == null) { // 아래 NPE 에러 해결 로직
-                int max = Arrays.stream(distance).max().getAsInt();
-                if (max == Integer.MAX_VALUE) {
-                    answer = -1;
-                } else {
-                    answer= max;
-                }
-                continue;
-            }
-            
-            for (Entry e : graph.get(cur.node)) { // NPE 에러 주의
-                int to = e.node;
-                int newDist = cur.time + e.time;
-                if (newDist < distance[to]) {
-                    distance[to] = newDist;
-                    q.offer(new Entry(to, newDist));
-                }
-            }
-
-        }
-        int max = Arrays.stream(distance).max().getAsInt();
-        
-        if (max == Integer.MAX_VALUE) {
-            answer = -1;
-        } else {
-            answer = max;
-        }
-        return answer;
-        
-    }
-    
-    class Entry implements Comparable<Entry>{
-        int node;
-        int time;
-        
-        public Entry(int node, int time) {
-            this.node = node;
-            this.time = time;
-        }
-        
-        @Override
-        public int compareTo(Entry o) {
-            return this.time - o.time;
-        }
+        return visitCount == n ? maxTime : -1;
     }
 }
