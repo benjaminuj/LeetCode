05d4ce5e25bd050fe7bcd967c607278c80bc4304
YuJin Cho <chosj1526@gmail.com>
2024-02-14 03:23:53 +0900

    Time: 12 ms (53.70%), Space: 46.1 MB (6.46%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,27 +1,63 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[][] arr = new int[nums.length][2];
-        for (int i = 0; i < nums.length; i++) {
-            arr[i][0] = nums[i];
-            arr[i][1] = i;
+        boolean duplicated = false;
+        Map<Integer, Edge> numsMap = new HashMap<>(); // 값, Edge(개수, 인덱스)
+        for (int i =0 ; i < nums.length; i++) {
+            numsMap.putIfAbsent(nums[i], new Edge(0, -1));
+            
+            numsMap.put(nums[i], new Edge(numsMap.get(nums[i]).cnt +1, i));
         }
         
-        Arrays.sort(arr, (o1, o2) -> {
-            return o1[0] - o2[0];
-        });
+        int[] answer = new int[2];
+        int diff = 0;
         
-        int l = 0, r = nums.length - 1;
+        for (int i =0; i < nums.length; i++) {
+            diff = target - nums[i];
+            numsMap.put(nums[i], new Edge(numsMap.get(nums[i]).cnt - 1, i));
+            
+            if (numsMap.containsKey(diff)) {
+                if (nums[i] == diff && numsMap.get(diff).cnt > 0) {
+                    duplicated = true;
+                    break;
+                } else if (nums[i] == diff && numsMap.get(diff).cnt == 0) {
+                    continue;
+                }
+                // int cnt = getValueCnt(diff, nums); 
+                // System.out.println(diff);
+                // if (cnt != 1) {
+                //     System.out.println(cnt);
+                //     duplicated = true;
+                //     break;
+                // }
+                System.out.println(i);
+                answer[0] = i;
+                answer[1] = numsMap.get(diff).index;
+                Arrays.sort(answer);
+                break;
+            }
+            numsMap.put(nums[i], new Edge(numsMap.get(nums[i]).cnt + 1, i));
+        }
         
-        while(l < r) {
-            if (arr[l][0] + arr[r][0] > target) {
-                r--;
-            } else if(arr[l][0] + arr[r][0] < target) {
-                l++;
-            } else {
-                return new int[]{arr[l][1], arr[r][1]};
+        if (duplicated) {
+            int idx = 0;
+            for (int i =0; i < nums.length; i++) {
+                if (nums[i] == diff) {
+                    answer[idx++] = i;
+                }
+                if (idx == 2) break;
             }
         }
         
-        return new int[]{-1, -1};
+        return answer;
+    }
+    
+    class Edge {
+        int cnt;
+        int index;
+        
+        public Edge(int cnt, int index) {
+            this.cnt = cnt;
+            this.index = index;
+        }
     }
 }
