cc9e9c66fd7c886236c40b7bc6fb2ac3c11522e9
YuJin Cho <chosj1526@gmail.com>
2024-02-16 15:31:44 +0900

    Time: 941 ms (37.32%), Space: 55 MB (44.15%) - LeetHub

--- 2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.java
+++ 2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.java
@@ -1,67 +1,48 @@
 class Solution {
     public int minimumDifference(int[] nums) {
-        int n = nums.length / 2;
-        Arrays.sort(nums);
-        int sum = 0;
-        for (int num : nums) sum += num;
-		
-		// split into first and second half (in fact we do not even need the sort, just easier to calculate upper bound this way)
-        int[] firstHalf = new int[n];
-        for (int i = 0; i < n; i++) {
-            firstHalf[i] = nums[i];
-        }
-        int[] secondHalf = new int[n];
-        for (int i = 0; i < n; i++) {
-            secondHalf[i] = nums[n + i];
-        }
+        int n = nums.length/2;
+        int sum = Arrays.stream(nums).sum();
+        double target = sum/2.0; // 더 정확하게 계산하기 위해 정수가 아닌 실수타입으로 하는건가요? 
+        List<List<Integer>> partialSums1 = getPartialSums(nums, 0, n);
+        List<List<Integer>> partialSums2 = getPartialSums(nums, n, n*2);
         
-        int firstSum = 0;
-        for (int num : firstHalf) firstSum += num;
-        
-        int minimal = Math.abs(sum - 2 * firstSum);
-        for (int i = 1; i < n; i++) {
-            int other = n - i; // pick i from first half and pick other from second half
-            List<Integer> firstList = new ArrayList<>();
-            helper(firstHalf, 0, 0, 0, i, firstList);
-            TreeSet<Integer> set = new TreeSet<>(firstList); // prepare for binary search
-            
-            List<Integer> secondList = new ArrayList<>();
-            helper(secondHalf, 0, 0, 0, other, secondList);
-            for (int second : secondList) {
-                int target = sum / 2 - second;
-                Integer floor = set.floor(target); // search for a smaller value
-                if (floor != null) {
-                    int curSum = second + floor;
-                    int curAbs = Math.abs(sum - curSum * 2);
-                    if (curAbs < minimal) {
-                        minimal = curAbs;
-                    }
-                }
-                Integer ceiling = set.ceiling(target); // search for a larget value
-                if (ceiling != null) {
-                    int curSum = second + ceiling;
-                    int curAbs = Math.abs(sum - curSum * 2);
-                    if (curAbs < minimal) {
-                        minimal = curAbs;
-                    }
+        double best = Double.MAX_VALUE;
+        for (int k = 0; k  <partialSums1.size(); k++) {
+            List<Integer> sums1 = partialSums1.get(k);
+            List<Integer> sums2 = partialSums2.get(partialSums2.size()-1-k);
+            for (int sum1 : sums1) {
+                double targetDiff = target - sum1;
+                int index = Collections.binarySearch(sums2, (int)targetDiff);
+                
+                if (index < 0) index = ((index +1) * -1); 
+                
+                if (index < sums2.size()) {
+                    best = Math.min(best, sum1 + sums2.get(index) - target);
                 }
             }
         }
-        
-        return minimal;
+        return Math.abs((int)(best*2.0));
     }
     
-    private void helper(int[] nums, int ptr, int pickedCount, int sum, int targetCount, List<Integer> vals) { // calculate all possible sums when picking targetCount from array
-        if (pickedCount > targetCount) return;
-        
-        if (pickedCount == targetCount) {
-            vals.add(sum);
-            return;
+    List<List<Integer>> getPartialSums(int[] nums, int start, int end) {
+        int len = end - start;
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i =0 ; i<= len; i++) {
+            result.add(new ArrayList<>());
         }
-        
-        if (ptr == nums.length) return;
-        
-        helper(nums, ptr + 1, pickedCount, sum, targetCount, vals);
-        helper(nums, ptr + 1, pickedCount + 1, sum + nums[ptr], targetCount, vals);
+        for (int mask =0; mask < (1 << len); mask++) {
+            int sum =0;
+            for (int i = 0; i < len; i++) {
+                if ((mask>>i & 1) == 1) {
+                    sum += nums[start+i];
+                }
+            }
+            int pickCount = Integer.bitCount(mask);
+            result.get(pickCount).add(sum);
+        }
+        for (List<Integer> sums : result) {
+            Collections.sort(sums);
+        }
+        return result;
     }
 }
