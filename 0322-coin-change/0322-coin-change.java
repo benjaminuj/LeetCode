04da3348372790bbe2b844e1af99d16081581a36
YuJin Cho <chosj1526@gmail.com>
2024-02-22 19:26:22 +0900

    Time: 16 ms (67.17%), Space: 44.2 MB (74.12%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,40 +1,27 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        if (amount == 0) {
-            return 0;
+        int[] answer = new int[amount+1];
+        Arrays.fill(answer, 100000);
+        for(int i =0; i < coins.length; i++) {
+            if (coins[i] > amount) continue;
+            answer[coins[i]] = 1;
         }
+        answer[0] = 0;
         
-        return bfs(coins, amount);
-    }
-    
-    public int bfs(int[] coins, int amount) {
-        Queue<Info> q = new ArrayDeque<>();
-        boolean[] visited = new boolean[amount+1];
-        
-        q.offer(new Info(0, amount));
-        while (!q.isEmpty()) {
-            Info now = q.poll();
-        
-            for (int i = 0; i < coins.length; i++) { 
-                int nextAmount = now.amount - coins[i];
-                if (nextAmount == 0) {
-                    return now.coinCnt + 1;                     
-                } else if (nextAmount > 0 && !visited[nextAmount]) {
-                    q.offer(new Info(now.coinCnt + 1, nextAmount));
-                    visited[nextAmount] = true;
+        for (int coin : coins) {
+            for (int i = 0; i < amount; i++) {
+                if (answer[i] == 100000) continue;
+                if (i+coin > amount) break;
+                
+                int temp = answer[i] + 1;
+                if (answer[i+coin] == 100000) answer[i+coin] = temp;
+                else {
+                    answer[i+coin] = Math.min(temp, answer[i+coin]);
                 }
             }
         }
-        return -1;
-    }
-}
-
-class Info {
-    int coinCnt;
-    int amount;
-    
-    public Info(int coinCnt, int amount) {
-        this.coinCnt = coinCnt;
-        this.amount = amount;
+        
+        if (answer[amount] == 100000) return -1;
+        return answer[amount];
     }
 }
