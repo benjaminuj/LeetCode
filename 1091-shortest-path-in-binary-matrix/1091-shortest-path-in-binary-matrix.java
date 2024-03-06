1473337e5f576664b72b180e08e63d54071480d8
YuJin Cho <chosj1526@gmail.com>
2024-03-06 20:11:41 +0900

    Time: 17 ms (59.61%), Space: 45.6 MB (43.52%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,53 +1,39 @@
 class Solution {
-    int dr[][] = {{0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}};
-    boolean[][] visited;
+    int n;
     
     public int shortestPathBinaryMatrix(int[][] grid) {
-        visited = new boolean[grid.length][grid[0].length];
+        n = grid.length;
+        if (grid[0][0] == 1) return -1;
+        if (grid[0][0] == 0 && n ==1) return 1;
         
-        if (grid[0][0] == 1 || grid[grid.length-1][grid[0].length-1] == 1) {
-            return -1;
-        }
         
-        return bfs(0,0, grid);
-    }
-    
-    public boolean isValid(int r, int c, int rLen, int cLen, int[][] grid) {
-        return ((r >= 0 && r < rLen && c >= 0 && c < cLen) && !visited[r][c] && grid[r][c] == 0);
-    }
-    
-    public int bfs(int r, int c, int[][] grid) {
-        Queue<Info> q = new ArrayDeque<>();
-        q.offer(new Info(r, c, 1));
-        visited[r][c] = true;
+        int[][] dr = {{0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}};
+        boolean[][] visited = new boolean[n][n];
+        Queue<int[]> q= new ArrayDeque<>();
         
-        while (!q.isEmpty()) {
-            Info now = q.poll();
+        q.add(new int[]{0, 0, 1});
+        visited[0][0] = true;
+        
+        while(!q.isEmpty()) {
+            int[] cur= q.poll();
+            int cnt = cur[2];
             
-            if (now.r == grid.length-1 && now.c == grid[0].length-1) {
-                return now.cnt;
-            }
             for (int[] d : dr) {
-                int nextR = now.r + d[0];
-                int nextC = now.c + d[1];
-            
-                if (isValid(nextR, nextC, grid.length, grid[0].length, grid)) {
-                    q.offer(new Info(nextR, nextC, now.cnt + 1));
-                    visited[nextR][nextC] = true;
+                int nr = cur[0] + d[0];
+                int nc = cur[1] + d[1];
+                
+                if (isValid(nr, nc) && !visited[nr][nc] && grid[nr][nc] == 0) {
+                    if (nr == n-1 && nc == n-1) return cnt+1;
+                    
+                    q.add(new int[]{nr, nc, cnt+1});
+                    visited[nr][nc] = true;
                 }
             }
         }
         return -1;
     }
-}
-
-class Info {
-    int r;
-    int c;
-    int cnt;
-    public Info(int r, int c, int cnt) {
-        this.r = r;
-        this.c = c;
-        this.cnt = cnt;
+    
+    public boolean isValid(int r, int c) {
+        return r >=0 && c >= 0 && r < n && c < n;
     }
 }
